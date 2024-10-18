54577790f6505ffdf36300d4d0e84c308994df15
Marcio123TreeBall <joejoejow10@gmail.com>
2024-10-19 07:35:28 +0900

    Novoooo2

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,8 +1,11 @@
 $(document).on("click", "#alerta", function(){
 
-    function retorno(){}
+    function retorno(){
+    }
     navigator.notification.alert("minha Mensagem", retorno, "Aviso!","Aceito");
 });
+
+
 $(document).on("click","#confirm", function(){
     function confirma(buttonIndex){
         if(buttonIndex =="1"){
@@ -12,4 +15,12 @@ $(document).on("click","#confirm", function(){
         }
     }
     navigator.notification.confirm("Escolha A ou B", confirma, "Escolha:", ['B','A']);
+});
+
+$(document).on("click","#beep", function(){
+    navigator.notification.beep(4);
+});
+
+$(document).on("click","#vibra", function(){
+    navigator.vibrate(3000);
 });
